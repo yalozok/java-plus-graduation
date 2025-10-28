884346ae2990b87de8a92f9b56de3dd3c62e21db
yalozok <olga.kozmovskaia@yandex.ru>
2025-10-28 15:34:20 +0100

    Update docker compose running

--- main-service/src/main/java/ru/practicum/explore/with/me/MainApp.java
+++ main-service/src/main/java/ru/practicum/explore/with/me/MainApp.java
@@ -3,7 +3,7 @@ package ru.practicum.explore.with.me;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"ru.practicum.explore.with.me", "ru.practicum.stat"})
+@SpringBootApplication(scanBasePackages = {"ru.practicum.explore.with.me", "ru.practicum.stats"})
 public class MainApp {
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
