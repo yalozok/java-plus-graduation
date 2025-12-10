8322243239e826bfd5053e042f79f3ca4fc45b27
yalozok <olga.kozmovskaia@yandex.ru>
2025-12-10 13:00:28 +0100

    Add request feign client

---
+++ core/interaction-api/src/main/java/ru/practicum/explore/with/me/interaction/api/client/request/RequestFeignConfig.java
@@ -0,0 +1,14 @@
+package ru.practicum.explore.with.me.interaction.api.client.request;
+
+import feign.Feign;
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+
+public class RequestFeignConfig {
+    @Bean
+    public Feign.Builder feignBuilder() {
+        return Feign.builder()
+                .errorDecoder(new CustomErrorDecoder())
+                .logLevel(Logger.Level.FULL);
+    }
+}
