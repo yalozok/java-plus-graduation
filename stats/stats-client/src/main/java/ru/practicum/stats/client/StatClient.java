60d8e769ba684760a2d534d86d84d2986dcfca4e
yalozok <olga.kozmovskaia@yandex.ru>
2025-12-22 22:27:58 +0500

    Update StatsClient

--- stats/stats-client/src/main/java/ru/practicum/stats/client/StatClient.java
+++ stats/stats-client/src/main/java/ru/practicum/stats/client/StatClient.java
@@ -1,14 +1,76 @@
 package ru.practicum.stats.client;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.ResponseEntity;
-import ru.practicum.stats.dto.ViewStats;
+import com.google.protobuf.Timestamp;
+import io.grpc.StatusRuntimeException;
+import lombok.RequiredArgsConstructor;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Service;
+import ru.practicum.ewm.stats.proto.ActionTypeProto;
+import ru.practicum.ewm.stats.proto.RecommendationControllerGrpc;
+import ru.practicum.ewm.stats.proto.UserActionControllerGrpc;
+import ru.practicum.ewm.stats.proto.UserActionProto;
+import ru.practicum.explore.with.me.logging.Loggable;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import java.time.Instant;
 
-public interface StatClient {
-    void createHit(HttpServletRequest request);
+@Service
+@RequiredArgsConstructor
+public class StatClient {
+    @GrpcClient("analyzer")
+    private RecommendationControllerGrpc.RecommendationControllerBlockingStub recommendationClient;
 
-    ResponseEntity<List<ViewStats>> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
+    @GrpcClient("controller")
+    private UserActionControllerGrpc.UserActionControllerBlockingStub actionClient;
+
+    @Loggable
+    public void sendViewAction(long userId, long eventId) {
+        UserActionProto action = UserActionProto.newBuilder()
+                .setUserId(userId)
+                .setEventId(eventId)
+                .setTimestamp(toProtoTimestamp(Instant.now()))
+                .setActionType(ActionTypeProto.ACTION_VIEW)
+                .build();
+        try {
+            actionClient.collectUserAction(action);
+        } catch (StatusRuntimeException e) {
+            throw new StatsClientException("Failed to send VIEW action for user" + userId, e);
+        }
+    }
+
+    @Loggable
+    public void sendRegisterAction(long userId, long eventId) {
+        UserActionProto action = UserActionProto.newBuilder()
+                .setUserId(userId)
+                .setEventId(eventId)
+                .setTimestamp(toProtoTimestamp(Instant.now()))
+                .setActionType(ActionTypeProto.ACTION_REGISTER)
+                .build();
+        try {
+            actionClient.collectUserAction(action);
+        } catch (StatusRuntimeException e) {
+            throw new StatsClientException("Failed to send REGISTER action for user" + userId, e);
+        }
+    }
+
+    @Loggable
+    public void sendLikeAction(long userId, long eventId) {
+        UserActionProto action = UserActionProto.newBuilder()
+                .setUserId(userId)
+                .setEventId(eventId)
+                .setTimestamp(toProtoTimestamp(Instant.now()))
+                .setActionType(ActionTypeProto.ACTION_LIKE)
+                .build();
+        try {
+            actionClient.collectUserAction(action);
+        } catch (StatusRuntimeException e) {
+            throw new StatsClientException("Failed to send LIKE action for user" + userId, e);
+        }
+    }
+
+    private Timestamp toProtoTimestamp(Instant instant) {
+        return Timestamp.newBuilder()
+                .setSeconds(instant.getEpochSecond())
+                .setNanos(instant.getNano())
+                .build();
+    }
 }
---
+++ stats/stats-client/src/main/java/ru/practicum/stats/client/StatsClientException.java
@@ -0,0 +1,7 @@
+package ru.practicum.stats.client;
+
+public class StatsClientException extends RuntimeException {
+    public StatsClientException(String message,  Throwable cause) {
+        super(message, cause);
+    }
+}
