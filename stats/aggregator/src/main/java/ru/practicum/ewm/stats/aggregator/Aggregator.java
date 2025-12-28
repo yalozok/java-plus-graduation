1b4bf26e0978ebdb381a38e98c5472d5ff6570c0
yalozok <olga.kozmovskaia@yandex.ru>
2025-12-28 12:03:32 +0500

    enable discovery client for analyzer and aggregator explicitly

--- stats/aggregator/src/main/java/ru/practicum/ewm/stats/aggregator/Aggregator.java
+++ stats/aggregator/src/main/java/ru/practicum/ewm/stats/aggregator/Aggregator.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import ru.practicum.ewm.stats.aggregator.config.KafkaProperties;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableConfigurationProperties(KafkaProperties.class)
+@EnableDiscoveryClient
 public class Aggregator {
     public static void main(String[] args) {
         SpringApplication.run(Aggregator.class, args);
--- stats/analyzer/src/main/java/ru/practicum/ewm/stats/analyzer/Analyzer.java
+++ stats/analyzer/src/main/java/ru/practicum/ewm/stats/analyzer/Analyzer.java
@@ -3,9 +3,11 @@ package ru.practicum.ewm.stats.analyzer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableDiscoveryClient
 public class Analyzer {
     public static void main(String[] args) {
         SpringApplication.run(Analyzer.class, args);
