2943aef8ace3f24a4e471d1a7cc83096330aef30
yalozok <olga.kozmovskaia@yandex.ru>
2025-12-28 11:21:49 +0500

    Update stats client

--- stats/stats-client/src/main/java/ru/practicum/stats/client/StatClient.java
+++ stats/stats-client/src/main/java/ru/practicum/stats/client/StatClient.java
@@ -5,13 +5,11 @@ import io.grpc.StatusRuntimeException;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
-import ru.practicum.ewm.stats.proto.ActionTypeProto;
-import ru.practicum.ewm.stats.proto.RecommendationControllerGrpc;
-import ru.practicum.ewm.stats.proto.UserActionControllerGrpc;
-import ru.practicum.ewm.stats.proto.UserActionProto;
+import ru.practicum.ewm.stats.proto.*;
 import ru.practicum.explore.with.me.logging.Loggable;
 
 import java.time.Instant;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -33,7 +31,7 @@ public class StatClient {
         try {
             actionClient.collectUserAction(action);
         } catch (StatusRuntimeException e) {
-            throw new StatsClientException("Failed to send VIEW action for user" + userId, e);
+            throw new StatsClientException("Failed to send VIEW action for user: " + userId, e);
         }
     }
 
@@ -48,7 +46,7 @@ public class StatClient {
         try {
             actionClient.collectUserAction(action);
         } catch (StatusRuntimeException e) {
-            throw new StatsClientException("Failed to send REGISTER action for user" + userId, e);
+            throw new StatsClientException("Failed to send REGISTER action for user: " + userId, e);
         }
     }
 
@@ -63,7 +61,7 @@ public class StatClient {
         try {
             actionClient.collectUserAction(action);
         } catch (StatusRuntimeException e) {
-            throw new StatsClientException("Failed to send LIKE action for user" + userId, e);
+            throw new StatsClientException("Failed to send LIKE action for user: " + userId, e);
         }
     }
 
@@ -72,5 +70,68 @@ public class StatClient {
                 .setSeconds(instant.getEpochSecond())
                 .setNanos(instant.getNano())
                 .build();
+    }
+
+    @Loggable
+    public Map<Long, Double> getInteractionsCount(List<Long> eventIds) {
+        InteractionsCountRequestProto request = InteractionsCountRequestProto
+                .newBuilder()
+                .addAllEventId(eventIds)
+                .build();
+        try {
+            Iterator<RecommendedEventProto> iterator = recommendationClient.getInteractionsCount(request);
+            Map<Long, Double> result = new HashMap<>();
+
+            while (iterator.hasNext()) {
+                RecommendedEventProto proto = iterator.next();
+                result.put(proto.getEventId(), proto.getScore());
+            }
+            return result;
+        } catch (Exception e) {
+            throw new StatsClientException("Failed to get rating for events", e);
+        }
+    }
+
+    @Loggable
+    public Map<Long, Double> getSimilarEvents(long userId, long eventId, int limit) {
+        SimilarEventsRequestProto request = SimilarEventsRequestProto
+                .newBuilder()
+                .setEventId(eventId)
+                .setUserId(userId)
+                .setMaxResults(limit)
+                .build();
+        try {
+            Iterator<RecommendedEventProto> iterator = recommendationClient.getSimilarEvents(request);
+            Map<Long, Double> result = new HashMap<>();
+
+            while (iterator.hasNext()) {
+                RecommendedEventProto proto = iterator.next();
+                result.put(proto.getEventId(), proto.getScore());
+            }
+            return result;
+        } catch (Exception e) {
+            throw new StatsClientException("Failed to get similar events for eventId " + eventId, e);
+        }
+    }
+
+    @Loggable
+    public Map<Long, Double> getRecommendationsForUser(long userId, int limit) {
+        UserPredictionsRequestProto request = UserPredictionsRequestProto
+                .newBuilder()
+                .setUserId(userId)
+                .setMaxResults(limit)
+                .build();
+        try {
+            Iterator<RecommendedEventProto> iterator = recommendationClient.getRecommendationsForUser(request);
+            Map<Long, Double> result = new HashMap<>();
+
+            while (iterator.hasNext()) {
+                RecommendedEventProto proto = iterator.next();
+                result.put(proto.getEventId(), proto.getScore());
+            }
+            return result;
+        } catch (Exception e) {
+            throw new StatsClientException("Failed to get recommendations for user: " + userId, e);
+        }
     }
 }
